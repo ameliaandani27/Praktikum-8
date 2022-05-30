9675acf6d9d0eddd93111f308e35aceffc9b982e
ameliaandani27 <ameliaandanii27@gmail.com>
2022-05-30 13:39:35 +0700

    Praktikum-8

---
+++ application/controllers/Mahasiswa.php
@@ -0,0 +1,29 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Mahasiswa extends CI_Controller {
+
+ public function index()
+    {
+        $this->load->model('mahasiswa_model','mhs1');
+        $this->mhs1->id=1;
+        $this->mhs1->nim='010001';
+        $this->mhs1->nama='Amelia';
+        $this->mhs1->gender='P';
+        $this->mhs1->ipk=3.85;
+
+ $this->load->model('mahasiswa_model','mhs2');
+ $this->mhs2->id=2;
+ $this->mhs2->nim='020001';
+ $this->mhs2->nama='Andani';
+ $this->mhs2->gender='L';
+ $this->mhs2->ipk=3.35;
+
+ $list_mhs = [$this->mhs1, $this->mhs2];
+ $data['list_mhs']=$list_mhs;
+ $this->load->view('layouts/header'); 
+ $this->load->view('mahasiswa/index',$data);
+ $this->load->view('layouts/footer');
+ }
+
+}
